71dfc31da2ca90dbdfc35e861c6a3bd5fa74d1bf
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-08-14 23:48:34 +0800

    finalized Student Schedule front-end

--- src/pages/ClassGenerated.js
+++ src/pages/ClassGenerated.js
@@ -7,6 +7,7 @@ import TextField from "@material-ui/core/TextField";
 import copy from "clipboard-copy";
 import MuiAlert from "@material-ui/lab/Alert";
 import Snackbar from "@material-ui/core/Snackbar";
+import { Link } from "react-router-dom";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -47,17 +48,29 @@ class ClassGenerated extends React.Component {
             variant="outlined"
             disabled
           />
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginTop: 10 }}
-            onClick={() => {
-              copy("www.studify.com/?data1=value1&data2=value2");
-              this.setState({ open: true });
-            }}
-          >
-            Copy
-          </Button>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginTop: 10 }}
+              onClick={() => {
+                copy("www.studify.com/?data1=value1&data2=value2");
+                this.setState({ open: true });
+              }}
+            >
+              Copy
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginTop: 10, marginLeft: 10 }}
+            >
+              <Link style={{ color: "white", textDecoration: "none" }} to="/">
+                Back to classes
+              </Link>
+            </Button>
+          </div>
+
           <Snackbar
             open={this.state.open}
             autoHideDuration={2000}
