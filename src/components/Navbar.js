db8a6368a8a1760fcb1a7305567e760b2b809c31
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-08-14 23:38:28 +0800

    finalized Navbar for Student Side

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,15 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import PersonIcon from "@material-ui/icons/Person";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,10 +26,15 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  large: {
+    width: theme.spacing(9),
+    height: theme.spacing(9),
+  },
 }));
 
 function Navbar() {
   const classes = useStyles();
+  const [state, setState] = React.useState(false);
 
   return (
     <div className={classes.root}>
@@ -31,6 +45,7 @@ function Navbar() {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            onClick={() => setState(true)}
           >
             <MenuIcon />
           </IconButton>
@@ -39,6 +54,39 @@ function Navbar() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer
+        style={{ width: 120 }}
+        anchor="left"
+        open={state}
+        width="50%"
+        onClose={() => setState(false)}
+      >
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          style={{ marginTop: 15 }}
+          direction="column"
+          flexWrap="wrap"
+        >
+          <Avatar className={classes.large}>
+            <PersonIcon fontSize="large" />
+          </Avatar>
+          <h2>Jack Jackson</h2>
+        </Grid>
+        <Divider variant="middle" />
+        <List style={{ width: 250 }} component="nav">
+          <ListItem></ListItem>
+          <ListItem button>
+            <ListItemIcon>🗓️</ListItemIcon>
+            <ListItemText primary="My classes" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>✉️</ListItemIcon>
+            <ListItemText primary="My sponsors" />
+          </ListItem>
+        </List>
+      </Drawer>
     </div>
   );
 }
